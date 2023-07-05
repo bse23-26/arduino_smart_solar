2769a069887043cd51d2f4972c97fb3c69c8b0ff
Timothy Kibalama <timkibalama@gmail.com>
2023-07-05 17:18:20 +0300

    finished

--- smart_solar.ino
+++ smart_solar.ino
@@ -1,34 +1,61 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
+#include <EEPROM.h>
+#include <ArduinoJson.h>
 
+//global objects and pins attached
 Servo servo;
-int pos = 0;
 SoftwareSerial bluetooth(10, 11); // RX, TX
-const int LED_BLUETOOTH = 8;
-const int servoPin = 9;
+// const int LED_BLUETOOTH = 8;
+const short servoPin = 8;
 const int ldrPin1 = A0;
 const int ldrPin2 = A1;
-const int ldrPin3 = A2;
+
+// const int currentPin = A2;
+// const int voltagePin = A3;
+
+const int line1 = 2;
+const int line2 = 3;
 
 int ldrPin1Status = 0;
 int ldrPin2Status = 0;
-int ldrPin3Status = 0;
+// int voltage = 0;
 
 const int minAngle = 0;
 const int maxAngle = 180;
 int currAngle = 90;
 
 const int baud = 9600;
-String dataCurrent = "";
-String dataPrepared = "";
+
+// unsigned long readingsTime = millis();
+
+struct Configuration {
+  char deviceId[37];
+  short powerlines;
+  short powerline1;
+  short powerline2;
+  float maxVoltage;
+  float minVoltage;
+  float nominalVoltage;
+  float rating;
+};
+
+Configuration configurations;
+
+bool populated;
+// bool readingsNeeded = true;
 
 void setup() {
   //pin setup
-  pinMode(LED_BLUETOOTH, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(LED_BLUETOOTH, OUTPUT);
+  // pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(line1, OUTPUT);
+  pinMode(line2, OUTPUT);
+  // pinMode(servoPin, OUTPUT);
   pinMode(ldrPin1, INPUT);
   pinMode(ldrPin2, INPUT);
-  pinMode(ldrPin3, INPUT);
+  // pinMode(currentPin, INPUT);
+  // pinMode(voltagePin, INPUT);
   servo.attach(servoPin);
   
   Serial.begin(baud);
@@ -36,23 +63,22 @@ void setup() {
     ;
   }
 
-  Serial.println("Goodnight moon!");
-
   //initialize
   bluetooth.begin(baud);
-  // bluetooth.println("Hello, world?");
   digitalWrite(LED_BUILTIN, LOW);
   servo.write(currAngle);
+  populated = false;
+  readConfigurations();
+  switchPowerlines();
   delay(15);
 }
 
 void loop() {
   rotateServo();
-  // rotate();
-  // interfaceBluetooth();
-  // rotate();
-  // servo.write(90);
-  // delay(15);
+  interfaceBluetooth();
+  // if(readingsNeeded){
+  // determineElectricReadings();
+  // }
 }
 
 long stringToInt(String str){
@@ -74,38 +100,60 @@ long stringToInt(String str){
 
 void interfaceBluetooth(){
   if (bluetooth.available()) {
+    // readingsNeeded = false;
     String dataReceived = bluetooth.readString();
+    // delay(100);
+    Serial.println(dataReceived);
     dataReceived.trim();
     if(dataReceived.length()>1){
       takeAction(dataReceived);
     }
   }
+}
 
-  if(dataPrepared.length()==0 && dataCurrent.length()>0){
-    dataPrepared = "" + dataCurrent;
-    dataCurrent = "";
-    bluetooth.println(dataPrepared);
-  }
+void sendBluetooth(String dataPrepared){
+  bluetooth.println(dataPrepared);
+  // delay(100);
 }
 
 void takeAction(String received){
-  int command = stringToInt(received.substring(0,2));
-
-  switch(command){
-    case 0:
-      dataPrepared = "";
-      break;
-    case 1:
-      break;
-    default:
-      break;
+  String comm = received.substring(0,3);
+  for(byte i=0;i<3;i++){
+    if(!isDigit(comm.charAt(i))) return;
   }
+  int command = stringToInt(comm);
+
+  //100 write configurations
+  //101 change state of the powerlines
+
+  //200 read configurations
+
+  if(command==100){
+      String sent = received.substring(3);
+      writeConfigurations(sent);
+      sendConfigurations();
+      return;
+  }
+
+  if(command==200){
+    sendConfigurations();
+  }
+}
+
+void sendConfigurations(){
+  String conf = readConfigurations();
+  makeCurrentData("configurations", conf);
+}
+
+// add backslash if data is string
+void makeCurrentData(String type, String data){
+  String dataCurrent = "{\"type\": \""+type+"\", \"data\": "+data+"}";
+  sendBluetooth(dataCurrent);
 }
 
 void rotateServo(){
   ldrPin1Status = analogRead(ldrPin1);
   ldrPin2Status = analogRead(ldrPin2);
-  ldrPin3Status = analogRead(ldrPin3);
 
   int max = ldrPin2Status;
   int maxPin = 2;
@@ -115,21 +163,127 @@ void rotateServo(){
     maxPin = 1;
   }
 
-  if(ldrPin3Status>max){
-    max = ldrPin3Status;
-    maxPin = 3;
-  }
-
-  if(maxPin==2){
+  if(abs(ldrPin1Status-ldrPin2Status)<=30){
     return;
   }
-  if(maxPin==1){
+
+  if(maxPin==1 && currAngle>minAngle){
     currAngle--;
-  }else{
+  }else if(currAngle<maxAngle){
     currAngle++;
   }
 
   servo.write(currAngle);
-  delay(150);
+  delay(15);
 }
 
+///////////////////////////////////////////////////////////////
+// void determineElectricReadings(){
+//   unsigned long newReadingsTime = millis();
+//   if((newReadingsTime-readingsTime)<2000){
+//     return;
+//   }
+//   readingsTime = newReadingsTime;
+//   Serial.println("readings...");
+//   voltage = analogRead(voltagePin);
+//   Serial.println("battery voltage is: "+String((voltage/1023.0)*25)+"V");
+
+//   voltage = analogRead(currentPin);
+//   Serial.println("battery current is: "+String((voltage/1023.0)*5)+"A");
+
+////////////////////////////////////////
+  // bluetooth.println("{\"type\": \"current\", \"data\": "+String((voltage/1023.0)*5)+"}");
+  // delay(100);
+
+//   float maxAnalogValue = 1015.0;
+// float maxVoltage = 25.0;
+  // float volt = (voltage*25.0)/1023.0;
+  // float diff1 = configurations.maxVoltage-configurations.minVoltage;
+  // float diff2 = volt-configurations.minVoltage;
+  
+  // Serial.println("percentage is:"+String((diff2/diff1)*100));
+  // Serial.println(String((voltage/maxAnalogValue)*maxVoltage));
+  // makeCurrentData("voltage", String(voltage));
+// }
+
+String readConfigurations(){
+  // String confStr;
+  // EEPROM.get(0, confStr);
+  // int strFirstIndex;
+  // String charNum = confStr.substring(0, strFirstIndex);
+  // int strLen = charNum.toInt();
+  // confStr = confStr.substring(strFirstIndex, strFirstIndex+strLen);
+
+  String output = "";
+  if(!populated){
+    EEPROM.get(0, configurations);
+    populated = true;
+    switchPowerlines();
+  }
+  else{
+    // doc["deviceId"] = configurations.deviceId;
+    // doc["powerlines"] = configurations.powerlines;
+    // doc["powerline1"] = configurations.powerline1;
+    // doc["powerline2"] = configurations.powerline2;
+    // doc["maxVoltage"] = configurations.maxVoltage;
+    // doc["minVoltage"] = configurations.minVoltage;
+    // doc["nominalVoltage"] = configurations.nominalVoltage;
+    // doc["rating"] = configurations.rating;
+
+    // \"deviceId\":\""+configurations.deviceId+"\",
+    output = "{\"powerlines\":"+String(configurations.powerlines)+",\"powerline1\":"+String(configurations.powerline1)+",\"powerline2\":"+String(configurations.powerline2)+",\"maxVoltage\":"+String(configurations.maxVoltage)+",\"minVoltage\":"+String(configurations.minVoltage)+",\"nominalVoltage\":"+String(configurations.nominalVoltage)+",\"rating\":"+String(configurations.rating)+"}";
+
+  }
+  return output;
+}
+
+void writeConfigurations(String confStr){
+  int str_len = confStr.length() + 1; 
+  char char_array[str_len];
+  confStr.toCharArray(char_array, str_len);
+
+  StaticJsonDocument<210> doc;
+
+  DeserializationError error = deserializeJson(doc, char_array, str_len);
+
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  configurations.powerline1 = doc["powerline1"];
+  configurations.powerline2 = doc["powerline2"];
+  configurations.maxVoltage = doc["maxVoltage"];
+  configurations.minVoltage = doc["minVoltage"];
+  configurations.nominalVoltage = doc["nominalVoltage"];
+  configurations.rating = doc["rating"];
+  switchPowerlines();
+
+  EEPROM.put(0, configurations);
+}
+
+void switchPowerlines(){
+  digitalWrite(line1, configurations.powerline1);
+  digitalWrite(line2, configurations.powerline2);
+}
+
+// setups
+// void setup() {
+//   String id = "0921a3e3-7a1f-4a0f-9ce9-8bd98bad7bc2";
+//   id.toCharArray(configurations.deviceId, 37);
+//   configurations.powerline1 = 0;
+//   configurations.powerline2 = 0;
+//   configurations.maxVoltage = 0;
+//   configurations.minVoltage = 0;
+//   configurations.nominalVoltage = 0;
+//   configurations.rating = 0;
+//   configurations.powerlines = 2;
+//   EEPROM.put(0, configurations);
+// }
+
+// void setup() {
+// }
+
+// void loop() {
+// }
